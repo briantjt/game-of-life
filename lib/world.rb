71f891479d521e9d3584a4a67f86dab0cf334d8a
Brian <tjtbrian@gmail.com>
2019-01-10 14:53:03 +0800

    World class
    - Fills each space with a cell instance with their correct x and y coordinates

--- lib/world.rb
+++ lib/world.rb
@@ -2,8 +2,21 @@ require 'cell'
 
 # World that controls behaviour of cells
 class World
-  attr_reader :grid
+  attr_reader :grid, :size
+
   def initialize(size)
     @grid = Array.new(size) { Array.new(size) }
+    @size = size
+    fill_grid
   end
+
+  def fill_grid
+    size.times do |y|
+      size.times do |x|
+        @grid[x][y] = Cell.new(rand <= 0.1, x, y)
+      end
+    end
+  end
+
+  
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -9,5 +9,21 @@ RSpec.describe 'World class' do
         expect(row.length).to eq 10
       end
     end
+
+    it 'Fills the grid with cells with their appropriate x and y coordinates' do
+      world = World.new(3)
+      world.grid.each do |row|
+        row.each do |cell|
+          expect(cell.class).to eq Cell
+        end
+      end
+      world.size.times do |y|
+        world.size.times do |x|
+          world.grid[x][y].x = x
+          world.grid[x][y].y = x
+        end
+      end
+
+    end
   end
 end
