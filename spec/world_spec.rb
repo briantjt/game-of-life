dbd0a169e57cc7a60a691b72a8512d6920687883
Brian <tjtbrian@gmail.com>
2019-01-10 14:39:30 +0800

    Refactored test to check all rows

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe 'World class' do
     it 'Creates a 2D grid of cells of n length' do
       world = World.new(10)
       expect(world.grid.length).to eq 10
-      expect(world.grid[0].length).to eq 10
+      world.grid.each do |row|
+        expect(row.length).to eq 10
+      end
     end
   end
 end
