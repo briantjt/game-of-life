602692806f0802b8773de3585cbc3717d283ed00
Brian <tjtbrian@gmail.com>
2019-01-14 17:12:37 +0800

    Changed formatting

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -3,7 +3,7 @@ require 'world'
 RSpec.describe 'World class' do
   context 'World creation' do
     it 'Creates a 2D grid of cells of n length' do
-      array = Array.new(10) {Array.new(10)}
+      array = Array.new(10) { Array.new(10) }
       world = World.new(array, 10)
       expect(world.grid.length).to eq 10
       world.grid.each do |row|
@@ -13,7 +13,7 @@ RSpec.describe 'World class' do
   end
   context 'Game state advancement' do
     before :example do
-      array = Array.new(3) {Array.new(3)}
+      array = Array.new(3) { Array.new(3) }
       @world = World.new(array, 3)
       @world.fill_grid(random: false)
       @world.tick
