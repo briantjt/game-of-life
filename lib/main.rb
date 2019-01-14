912a4965a84d6c4b227847d5b0d68dd2afbdb884
Brian <tjtbrian@gmail.com>
2019-01-14 11:23:08 +0800

    Added comments on printing escape sequences

--- lib/main.rb
+++ lib/main.rb
@@ -4,8 +4,10 @@ def game
   world = World.new(40)
   world.fill_grid
   system 'clear'
+  # Saves cursor position to be at the start of the console
   print "\033[s"
   loop do
+    # Moves cursor back to the saved position i.e. the beginning
     print "\033[u"
     puts "Generation: #{world.generation}"
     print world
