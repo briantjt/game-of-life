a4481227d595e636f0dd63518c41a4e82872ed8d
Brian <tjtbrian@gmail.com>
2019-01-16 16:29:25 +0800

    Refactor stdout for game class into its own method

--- lib/game.rb
+++ lib/game.rb
@@ -20,12 +20,18 @@ class Game
     # Saves cursor position to be at the start of the console
     print "\e[s"
     loop do
-      # Moves cursor back to the saved position i.e. the beginning
-      print "\e[u"
-      puts "Generation: #{world.generation}"
-      print world
+      print_world
       world = world.next_gen
       sleep(1)
     end
+  end
+
+  private
+
+  def print_world(world)
+    # Moves cursor back to the saved position i.e. the beginning
+    print "\e[u"
+    puts "Generation: #{world.generation}"
+    print world
   end
 end
