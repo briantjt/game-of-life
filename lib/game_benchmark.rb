fb4eb3d44539ab2dabdbe5c5e997fc9a54afeafa
Brian <tjtbrian@gmail.com>
2019-01-17 13:59:14 +0800

    Create benchmark files

---
+++ lib/game_benchmark.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require_relative 'world'
+
+# Initializes the world and outputs the state to the console
+class Game
+  def initialize(size)
+    @size = size
+  end
+
+  def start
+    matrix = Array.new(@size) { Array.new(@size) }
+    @size.times do |row|
+      @size.times do |column|
+        matrix[row][column] = Cell.new(rand < 0.2, row, column)
+      end
+    end
+    world = World.new(matrix, @size, 1)
+    system 'clear'
+    # Saves cursor position to be at the start of the console
+    print "\e[s"
+    loop do
+      print_world
+      world = world.next_gen
+      sleep(1)
+    end
+  end
+
+  private
+
+  def print_world(world)
+    # Moves cursor back to the saved position i.e. the beginning
+    print "\e[u"
+    puts "Generation: #{world.generation}"
+    print world
+  end
+end
