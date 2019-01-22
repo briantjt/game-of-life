9fef04f2375329c681a3e6db06945715f69cbf2e
Brian <tjtbrian@gmail.com>
2019-01-22 15:18:20 +0800

    Create game class

---
+++ lib/game.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require_relative 'world'
+
+# Starts the world and loops it
+class Game
+  def initialize(size)
+    @size = size
+  end
+
+  def start
+    matrix = Array.new(@size) { Array.new(@size) }
+    world = World.new(matrix, @size)
+    world.fill_grid
+    system 'clear'
+    # Saves cursor position to be at the start of the console
+    print "\e[s"
+    loop do
+      # Moves cursor back to the saved position i.e. the beginning
+      print "\e[u"
+      puts "Generation: #{world.generation}"
+      print world
+      world.tick
+      sleep(0.5)
+    end
+  end
+end
