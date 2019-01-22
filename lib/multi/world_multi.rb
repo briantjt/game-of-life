f38f1d7412012a56a5e8c24964e52841743d3f57
Brian <tjtbrian@gmail.com>
2019-01-22 16:26:29 +0800

    Add correct multithreaded world class

--- lib/multi/world_multi.rb
+++ lib/multi/world_multi.rb
@@ -12,13 +12,25 @@ class WorldMulti
 
   def next_gen
     next_gen_matrix = Array.new(@size) { Array.new(@size) }
-    @size.times do |row|
-      @size.times do |column|
-        cell_alive = @matrix[row][column].still_alive(@matrix, @size)
-        next_gen_matrix[row][column] = CellShared.new(cell_alive, row, column)
+    thread1 = Thread.new do
+      (@size / 2).times do |row|
+        @size.times do |column|
+          cell_alive = @matrix[row][column].still_alive(@matrix, @size)
+          next_gen_matrix[row][column] = Cell.new(cell_alive, row, column)
+        end
       end
     end
-    WorldMulti.new(next_gen_matrix, @size, @generation + 1)
+    thread2 = Thread.new do
+      Range.new(@size / 2, @size - 1).each do |row|
+        @size.times do |column|
+          cell_alive = @matrix[row][column].still_alive(@matrix, @size)
+          next_gen_matrix[row][column] = Cell.new(cell_alive, row, column)
+        end
+      end
+    end
+    thread1.join
+    thread2.join
+    World.new(next_gen_matrix, @size, @generation + 1)
   end
 
   def to_s
