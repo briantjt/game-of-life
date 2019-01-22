7b3452019e941ab004ba7920482f5eaa1717d269
Brian <tjtbrian@gmail.com>
2019-01-22 16:00:54 +0800

    GameShared.start now takes the world class as an argument

--- lib/shared/game_shared.rb
+++ lib/shared/game_shared.rb
@@ -1,6 +1,3 @@
-#!/usr/bin/env ruby
-
-require_relative 'world'
 
 # Initializes the world and outputs the state to the console
 # Class shared by multithreaded and immutable implementations
@@ -9,14 +6,14 @@ class GameShared
     @size = size
   end
 
-  def start
+  def start(world_class)
     matrix = Array.new(@size) { Array.new(@size) }
     @size.times do |row|
       @size.times do |column|
-        matrix[row][column] = Cell.new(rand < 0.2, row, column)
+        matrix[row][column] = CellShared.new(rand < 0.2, row, column)
       end
     end
-    world = World.new(matrix, @size, 1)
+    world = world_class.new(matrix, @size, 1)
     system 'clear'
     # Saves cursor position to be at the start of the console
     print "\e[s"
