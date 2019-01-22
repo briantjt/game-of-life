647f626e8052f5af224b0c994cfc1514d1db4961
Brian <tjtbrian@gmail.com>
2019-01-22 16:56:34 +0800

    Change tests to call correct classes

--- spec/immutable/world_spec.rb
+++ spec/immutable/world_spec.rb
@@ -1,11 +1,11 @@
-require 'world'
-require 'cell'
+require_relative '../../lib/immutable/world_immutable.rb'
+require_relative '../../lib/shared/cell_shared.rb'
 
 RSpec.describe 'World class' do
   context 'World creation' do
     it 'Creates a matrix of n length' do
       matrix = Array.new(10) { Array.new(10) }
-      world = World.new(matrix, 10, 1)
+      world = WorldImmutable.new(matrix, 10, 1)
       expect(world.matrix.length).to eq 10
       world.matrix.each do |row|
         expect(row.length).to eq 10
@@ -17,10 +17,10 @@ RSpec.describe 'World class' do
       matrix = Array.new(3) { Array.new(3) }
       3.times do |row|
         3.times do |column|
-          matrix[row][column] = Cell.new(false, row, column)
+          matrix[row][column] = CellShared.new(false, row, column)
         end
       end
-      @world = World.new(matrix, 3, 1)
+      @world = WorldImmutable.new(matrix, 3, 1)
     end
 
     it 'Generation counter gets incremented per evolution' do
@@ -32,18 +32,18 @@ RSpec.describe 'World class' do
     matrix1 = Array.new(3) { Array.new(3) }
     3.times do |row|
       3.times do |column|
-        matrix1[row][column] = Cell.new(false, row, column)
+        matrix1[row][column] = CellShared.new(false, row, column)
       end
     end
     matrix2 = Array.new(3) { Array.new(3) }
     3.times do |row|
       3.times do |column|
-        matrix2[row][column] = Cell.new(false, row, column)
+        matrix2[row][column] = CellShared.new(false, row, column)
       end
     end
-    matrix2[0][0] = Cell.new(true, 0, 0)
-    world1 = World.new(matrix1, 3, 1)
-    world2 = World.new(matrix2, 3, 1)
+    matrix2[0][0] = CellShared.new(true, 0, 0)
+    world1 = WorldImmutable.new(matrix1, 3, 1)
+    world2 = WorldImmutable.new(matrix2, 3, 1)
     expect(world1.to_s).to eq ". . .\n. . .\n. . .\n"
     expect(world2.to_s).to eq "0 . .\n. . .\n. . .\n"
   end
